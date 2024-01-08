0f9b66a3ff9a0f6cbf5b9357daa689bcdfbac94e
akil-bit <akilaps06@gmail.com>
2024-01-08 12:19:22 +0530

    fourth commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ToDoList from "./Components/ToDoList";
+import "./Components/ToDoList.jsx";
+import "./Components/ToDoList.css";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <ToDoList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/ToDoList.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import './ToDoList.css';
+
+const ToDoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, newTask]);
+      setNewTask('');
+    }
+  };
+
+  const removeTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div className="todo-list">
+      <h1>TODO LIST</h1>
+      <div className="add-task">
+        <input
+          type="text"
+          placeholder="add a task"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button className="btn" onClick={addTask}>Add</button>
+      </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => removeTask(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ToDoList;
